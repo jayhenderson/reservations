977742ab750acc9d4e4e891e3735450eb707faee
Jay Henderson <jjhenderson@gmail.com>
2017-05-19 23:38:41 -0500

    Integrate data into controller

--- src/main/java/com/j/controllers/ReservationController.java
+++ src/main/java/com/j/controllers/ReservationController.java
@@ -1,8 +1,20 @@
 package com.j.controllers;
 
+import com.j.business.domain.RoomReservation;
+import com.j.business.service.ReservationService;
+import com.j.models.data.ReservationDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by J on 5/17/2017.
@@ -11,9 +23,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/reservations")
 public class ReservationController {
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Autowired
+    private ReservationService reservationService;
+
 
     @RequestMapping(method = RequestMethod.GET)
-    public String getReservations(){
+    public String getReservations(@RequestParam(value = "date", required = false)String dateString, Model model){
+        Date date = null;
+        if (null != dateString) {
+            try {
+                date = DATE_FORMAT.parse(dateString);
+            } catch (ParseException pe) {
+                date = new Date();
+            }
+        } else {
+            date = new Date();
+        }
+
+        List<RoomReservation> roomReservationList = this.reservationService.getRoomReservationsForDate(date);
+        model.addAttribute("roomReservations", roomReservationList);
+
+
         return "reservations";
     }
 
